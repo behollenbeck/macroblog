792f03c0632cd78eca66b37dcafcdbc94e7f299b
elsewares <brian@elsewares.org>
2011-12-29 11:45:30 -0600

    Added in CakePHP OAuth components for Twitter API.

---
+++ app/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $macroblog = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'port' => 3306,
+		'login' => 'root',
+		'password' => 'Saffah1',
+		'database' => 'macroblog',
+	);
+}
